5966a5c5875599e3fdc8834364509c1775d12e15
odontologista <107404047+odontologista@users.noreply.github.com>
2022-06-13 07:56:15 -0300

    Update quizfunnel.js

--- quizfunnel.js
+++ quizfunnel.js
@@ -1,2 +1,8 @@
 // Show → Hide Content
 setTimeout(function() {$('.odnHide').attr("style", "display: none !important")}, 5000);
+
+// Hide → Show Content
+setTimeout(function() {$('.odnHide').attr("style", "display: block !important")}, 10000);
+
+// Cookie 365 Days
+if(/(^|;)\s*cookie_lp=/.test(document.cookie)){var divs=document.querySelectorAll(".odnhide");[].forEach.call(divs,function(e){e.style.setProperty("display", "block", "important")})}else document.cookie="cookie_lp=true; max-age=31536000";
