c08d3ee77db9aa9e8273a66aa8857267aebeeb35
odontologista <107404047+odontologista@users.noreply.github.com>
2022-06-27 14:50:05 -0300

    v1.3.2.4 fireFbq+stuff

--- quizfunnel.js
+++ quizfunnel.js
@@ -1,23 +1,22 @@
-console.log('v1.3.1.3 checkClinica');
+console.log('v1.3.2.4 fireFbq+stuff');
 
 // ----------------------
 // BASIC VARIABLES
 // ----------------------
 
 let urlLead = 'lead';
-let urlQuiz = 'quiz';
+let urlQuiz = 'quiz'; // Esse é só da página do quiz como funil, senão é 'form'
+let urlFone = 'fone';
+
 let urlForm = 'form';
+
 let urlResultados = 'resultados';
+
 let urlRedirecionando = 'redirecionando';
 
 let currentUrl = window.location.host + window.location.pathname;
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-
-// Form Append function for Active Campaign Forms
-function formAppend(formField, inputValue) {
-  $('form').append(`<input type="hidden" name="field[` + formField + `]" value="` + inputValue + `">`);
-}
 
 
 // ----------------------
@@ -26,96 +25,175 @@ function formAppend(formField, inputValue) {
 
 // Verificar se nome da clínica existe
 function checkClinica() {
+
   if (typeof nomeClinica !== 'undefined' && nomeClinica) {
-    console.log('Nome da Clínica: ' + nomeClinica);
     formAppend(40, nomeClinica);
   }
+
   else {
-    console.log('Nome da Clínica NÃO estava definido.');
-    let nomeClinica = 'Sem clínica';
-    console.log(nomeClinica);
+    console.warn('nomeClinica NÃO estava definido.');
+    let nomeClinica = 'nomeClinica vazio';
     formAppend(40, nomeClinica);
   }
+
+}
+
+// formAppend function for Active Campaign Forms
+function formAppend(formField, inputValue) {
+  $('form').append(`<input type="hidden" name="field[` + formField + `]" value="` + inputValue + `">`);
+  console.log('formField = ' + formField + ". inputValue = " + inputValue);
+}
+
+// Form Append geral
+function formAllAppends() {
+
+  // Dados básicos
+  formAppend(12, `${urlParams.get('utm_source')}`);
+  formAppend(11, `${urlParams.get('utm_campaign')}`);
+  formAppend(13, `${urlParams.get('utm_term')}`);
+  formAppend(15, `${urlParams.get('utm_medium')}`);
+  formAppend(14, `${urlParams.get('utm_content')}`);
+  formAppend(16, currentUrl);
+
+  // Verifica nomeClinica, senão define e envia
+  checkClinica();
+
+}
+
+// fireFbq = função pra disparar todas conversões
+function fireFbq(eventName, value, contentName) {
+  fbq("track", eventName, {
+    currency: "BRL",
+    value: value,
+    content_name: contentName
+  });
+  console.log("fireFbq : " + eventName + ", $" + value + ", " + contentName);
 }
 
 
 // ----------------------
-// FBQ PURCHASES
+// DOCUMENT.READY
 // ----------------------
 
-setTimeout(() => {
+$(document).ready(function () {
 
-  // Verifica ignorePurchase
-  if (typeof ignorePurchase !== 'undefined' && ignorePurchase) {
-    console.log('ignore Purchase true');
-  }
+  // ----------------------
+  // FBQ PURCHASES
+  // ----------------------
 
-  // Purchase por página
-  else {
-    console.log('ignore Purchase false');
+  setTimeout(() => {
 
-    // Quiz || Lead = $5 (optin básico)
-    if (window.location.href.includes(urlQuiz) || window.location.href.includes(urlLead)) {
-      console.log(`fbq Purchase: \$5, Fez Optin. URL: ${urlQuiz} || ${urlLead}`);
-      fbq('track', 'Purchase', { currency: 'BRL', value: 5.00, content_name: 'Fez Optin' });
+    // Verifica ignorePurchase
+    if (typeof ignorePurchase !== 'undefined' && ignorePurchase) {
+      console.log('ignorePurchase = true. Não disparar conversão.');
     }
 
-    // Form = $10 + on.submit(x) (porque é geralmente 3º página)
-    if (window.location.href.includes(urlForm)) {
-      console.log(`fbq Purchase: \$10, Chegou no Form. URL: ${urlForm}`);
-      fbq('track', 'Purchase', { currency: 'BRL', value: 10.00, content_name: 'Chegou no Form' });
+    // Purchase por página
+    else {
+
+      // Quiz || Lead = $5 (optin básico)
+      if (window.location.href.includes(urlQuiz) || window.location.href.includes(urlLead)) {
+        fireFbq('Purchase', 5, 'Optin Só E-mail');
+      }
+
+      // Inclui fone = $20
+      if (window.location.href.includes(urlFone)) {
+        fireFbq('Purchase', 20, 'Optin Com Telefone');
+      }
+
+      // Form = $20 + variável (on.submit(x), porque é geralmente 3º página)
+      if (window.location.href.includes(urlForm)) {
+        fireFbq('Purchase', 20, 'Chegou no Form');
+      }
+
+      // Resultados = $30 (preencheu quiz)
+      if (window.location.href.includes(urlResultados)) {
+        fireFbq('Purchase', 30, 'Preencheu Quiz');
+      }
+
+      // Redirecionanndo pro Whats = $50
+      if (window.location.href.includes(urlRedirecionando)) {
+        fireFbq('Purchase', 50, 'Contato WhatsApp');
+      }
+
+      // Purchase por tempo na página = $75
+      var seconds = 300; // 5 minutos
+      setTimeout(function () {
+        fireFbq('Purchase', 75, 'Tempo 5 mins');
+      }, seconds * 1000);
+
     }
 
-    // Resultados = $20 (preencheu quiz)
-    if (window.location.href.includes(urlResultados)) {
-      console.log(`fbq Purchase: \$20, Preencheu Quiz. URL: ${urlResultados}`);
-      fbq('track', 'Purchase', { currency: 'BRL', value: 20.00, content_name: 'Preencheu Quiz' });
-    }
-
-    // Redirecionanndo pro Whats = $50
-    if (window.location.href.includes(urlRedirecionando)) {
-      console.log(`fbq Purchase: \$50, Contato WhatsApp. URL: ${urlRedirecionando}`);
-      fbq('track', 'Purchase', { currency: 'BRL', value: 50.00, content_name: 'Contato WhatsApp' });
-    }
-  }
-}, 200);
+  }, 200);
 
 
-// Tempo na página
-var seconds = 300;
-setTimeout(function () {
-  fbq('track', 'Purchase', { currency: 'BRL', value: 60.00, content_name: 'Tempo 5 mins' });
-}, seconds * 1000);
+  // ----------------------
+  // REPLACEMENTS
+  // ----------------------
+
+  // Heading #1 (#h1)
+  $("b:contains('#h1')").each(function () {
+    $(this).addClass("h1Class");
+    $(this).parent().css("text-align", "center");
+    $(this).text(function () {
+      return $(this).text().replace("#h1", "");
+    });
+  });
+
+  // Horizontal Line (<hr />)
+  $('div.hrLine').find('div').eq(1).html('<hr />');
 
 
-// ----------------------
-// DADOS UTM & REDIRECT
-// ----------------------
+  // ----------------------
+  // DADOS UTM & REDIRECT
+  // ----------------------
 
-$(document).ready(() => {
-
-  // Optin com dados UTM - #optinInicial
-  $('#optinInicial button.btn').on('click', () => {
+  // Optin com dados UTM
+  $('#optinInicial button.btn, button.appendUrlParams').on('click', () => {
 
     // Dados básicos
-    $('form').append(`<input type="hidden" name="field[12]" value="${urlParams.get('utm_source')}">`);
-    $('form').append(`<input type="hidden" name="field[11]" value="${urlParams.get('utm_campaign')}">`);
-    $('form').append(`<input type="hidden" name="field[13]" value="${urlParams.get('utm_term')}">`);
-    $('form').append(`<input type="hidden" name="field[15]" value="${urlParams.get('utm_medium')}">`);
-    $('form').append(`<input type="hidden" name="field[14]" value="${urlParams.get('utm_content')}">`);
-    $('form').append(`<input type="hidden" name="field[16]" value="${currentUrl}">`);
-
-    // Verifica nomeClinica, senão define e envia
-    checkClinica();
+    formAllAppends();
 
     // Check if email is valid & redirect → /quiz?email=%email%
     if ($('input[type="email"]').val().includes('@') && $('input[type="email"]').val().includes('.')) {
+
+      // Redirect optinToQuiz → Quiz
       if ($('button').hasClass('optinToQuiz')) {
         setTimeout(() => {
           window.location.href = window.location.origin + '/quiz?email=' + $('input[type="email"]').val();
         }, 1000);
       }
+
+      // Posso adicionar outros redirects aqui daí se precisar passar e-mail. Mas pegando por cookie não vai precisar mais.
+
     }
+  });
+
+  //  Popup optin com dados UTM = button.abrirPopup
+  $('button.abrirPopup').on('click', () => {
+    console.log('Clicou button.abrirPopup');
+
+    // Esperar botão ficar disponível.
+    setTimeout(() => {
+
+      // Aqui é ok ser botão genérico porque o botão do popup tá ali antes.
+      $('button.btn').on('click', () => {
+
+        console.log('Clicou button.btn');
+
+        // Dados básicos
+        formAllAppends();
+
+        // Check if email is valid & redirect → /quiz?email=%email%
+        if ($('input[type="email"]').val().includes('@') && $('input[type="email"]').val().includes('.')) {
+          if ($('button').hasClass('optinToQuiz')) {
+            setTimeout(() => {
+              window.location.href = window.location.origin + '/quiz?email=' + $('input[type="email"]').val();
+            }, 1000);
+          }
+        }
+      });
+    }, 100);
   });
 
 
@@ -126,20 +204,24 @@ $(document).ready(() => {
   // Verificar se está em uma Quiz ou Form pela URL
   if (window.location.href.includes(urlQuiz) || window.location.href.includes(urlForm)) {
 
+    console.log('urlQuiz ou urlForm está presente.');
+
     // Valores das Respostas
     var soma = 0;
     //Define os pesos por questão:
     var pesos = {
       //"field"
+      //"resposta" : "peso"
+
       // Situação Atual
       "21": {
-        //"resposta" : "peso"
         "1": "5",
-        "2": "20",
-        "3": "5",
+        "2": "25",
+        "3": "15",
         "4": "10",
         "5": "5"
       },
+
       // Incômodos
       "25": {
         "1": "5",
@@ -147,6 +229,7 @@ $(document).ready(() => {
         "3": "5",
         "4": "5"
       },
+
       // Motivação
       "26": {
         "1": "5",
@@ -154,26 +237,29 @@ $(document).ready(() => {
         "3": "5",
         "4": "5"
       },
+
       // Impedimentos
       "22": {
-        "1": "0",
-        "2": "10",
-        "3": "10",
-        "4": "15",
-        "5": "15",
-        "6": "20"
+        "1": "0", // Custo
+        "2": "10", // Medo
+        "3": "10", // Tempo
+        "4": "15", // Desconhecia
+        "5": "15", // Dentistas de Confiança
+        "6": "25" // Estou pronto!
       },
+
       // Finanças
       "23": {
         "1": "-20",
         "2": "10",
         "3": "40"
       },
+
       // Urgência
       "24": {
-        "1": "0",
+        "1": "-10",
         "2": "5",
-        "3": "20"
+        "3": "30"
       }
     }
 
@@ -206,8 +292,7 @@ $(document).ready(() => {
             }
 
             // Send purchase track event to FB
-            console.log('Quiz Score: ' + soma);
-            fbq('track', 'Purchase', { currency: 'BRL', value: soma, content_name: 'Respostas Quiz' });
+            fireFbq('Purchase', soma, 'Respostas Quiz');
 
             // Verifica se telefone tem dígitos e envia dados
             if (/\d/.test($('input[id="phone"]').val())) {
@@ -218,10 +303,21 @@ $(document).ready(() => {
               // Manda soma junto
               formAppend(42, soma)
 
-              // Quiz || Form → /resultados
-              setTimeout(() => {
-                window.location.href = window.location.origin + '/resultados';
-              }, 1000);
+              // Quiz → /resultados
+              if (window.location.href.includes(urlQuiz)) {
+                console.log('urlQuiz → /resultados');
+                setTimeout(() => {
+                  window.location.href = window.location.origin + '/resultados';
+                }, 1000);
+              }
+
+              // Form → /resultados-agende
+              if (window.location.href.includes(urlForm)) {
+                console.log('urlForm → /resultados-agende');
+                setTimeout(() => {
+                  window.location.href = window.location.origin + '/resultados-agende';
+                }, 1000);
+              }
             }
 
             // Reseta soma
@@ -233,17 +329,18 @@ $(document).ready(() => {
     }, 100);
   }
 
+
+  // ----------------------
+  // SHOW / HIDE CONTENT
+  // ----------------------
+
+  // Show → Hide Content
+  setTimeout(function () { $('.odnShow').attr("style", "display: none !important") }, 5000);
+
+  // Hide → Show Content
+  setTimeout(function () { $('.odnHide').attr("style", "display: block !important") }, 7000);
+
+  // Cookie 365 Days
+  // if(/(^|;)\s*cookie_lp=/.test(document.cookie)){var divs=document.querySelectorAll(".odnhide");[].forEach.call(divs,function(e){e.style.setProperty("display", "block", "important")})}else document.cookie="cookie_lp=true; max-age=31536000";
+
 });
-
-// ----------------------
-// SHOW / HIDE CONTENT
-// ----------------------
-
-// Show → Hide Content
-setTimeout(function () { $('.odnShow').attr("style", "display: none !important") }, 5000);
-
-// Hide → Show Content
-setTimeout(function () { $('.odnHide').attr("style", "display: block !important") }, 7000);
-
-// Cookie 365 Days
-// if(/(^|;)\s*cookie_lp=/.test(document.cookie)){var divs=document.querySelectorAll(".odnhide");[].forEach.call(divs,function(e){e.style.setProperty("display", "block", "important")})}else document.cookie="cookie_lp=true; max-age=31536000";
